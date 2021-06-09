7d1f530de72d9745503a3f7cdb316ae14bca1d69
hellcassius <caiorogerio.santos@gmail.com>
2021-06-09 18:41:14 -0300

    fix simple_realized_trips.csv

--- tests/test_gtfs_to_realized_trips.py
+++ tests/test_gtfs_to_realized_trips.py
@@ -5,34 +5,36 @@ import numpy as np
 from pathlib import Path
 
 sys.path.append("../")
-from rgtfs import io
-import rgtfs
+import rGTFS
 
 
 def round_distance_and_average_speed(df):
     df["distance"] = df["distance"].apply(lambda x: np.round(x, decimals=1))
-    df["average_speed"] = df["average_speed"].aply(lambda x: np.round(x, decimals=1))
+    df["average_speed"] = df["average_speed"].apply(lambda x: np.round(x, decimals=1))
     return df
 
 
 @pytest.fixture
 def sample_data(tmpdir_factory):
-    return Path("sample_data")
+    return Path(".") / "sample_data"
 
 
 @pytest.fixture
-def gtfs_path(sample_data):
+def sample_gtfs_path(sample_data):
     return sample_data / "sample_gtfs"
 
 
 @pytest.fixture
 def simple_realized_trips(sample_data):
-    return sample_data / "sample_gtfs" / "simple_realized_trips.csv"
+    return sample_data / "simple_realized_trips.csv"
 
 
 @pytest.fixture
-def sample_realized_trips(simple_realized_trips):
-    return pd.read_csv(simple_realized_trips)
+def sample_realized_trips(sample_data):
+    csv_path = sample_data / "simple_realized_trips"
+    with open(sample_data, "r") as f:
+        rt = pd.read_csv(f)
+    return rt
 
 
 def test_simple_gtfs_to_realized(sample_gtfs_path, sample_realized_trips):
