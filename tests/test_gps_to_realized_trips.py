408f387cde97bdbc43076235fdcc206ae5da8c7d
hellcassius <caiorogerio.santos@gmail.com>
2021-06-11 17:39:15 -0300

    more sample csv files and functional test_rt_from_gps

--- tests/test_gps_to_realized_trips.py
+++ tests/test_gps_to_realized_trips.py
@@ -15,7 +15,7 @@ def sample_data_path():
 
 @pytest.fixture
 def sample_gps_path(sample_data_path):
-    return sample_data_path /"simple_csv"/ "simple_gps.csv"
+    return sample_data_path / "simple_csv" / "simple_gps.csv"
 
 
 @pytest.fixture
@@ -30,9 +30,12 @@ def rgtfs_path(sample_data_path):
 
 @pytest.fixture
 def simple_rt_from_gps(sample_data_path):
-    csv_path = sample_data_path /"simple_csv" /"simple_rt_from_gps.csv"
+    csv_path = sample_data_path / "simple_csv" / "simple_rt_from_gps.csv"
     with open(csv_path, "r") as f:
         df = pd.read_csv(f, parse_dates=["arrival_datetime", "departure_datetime"])
+    df["route_id"] = df["route_id"].astype("string")
+    df["trip_id"] = df["trip_id"].astype("string")
+    df["service_id"] = df["service_id"].astype("string")
     return df
 
 
@@ -40,5 +43,7 @@ def test_gps_to_realized_trips(
     sample_gps_path, sample_gtfs_path, rgtfs_path, simple_rt_from_gps
 ):
     rt = simple.main(sample_gtfs_path, sample_gps_path, rgtfs_path)
-
+    rt["route_id"] = rt["route_id"].astype("string")
+    rt["trip_id"] = rt["trip_id"].astype("string")
+    rt["service_id"] = rt["service_id"].astype("string")
     assert rt.equals(simple_rt_from_gps)
