0daf5bcd5057f9d2206d7c0fc8dd972f864d86be
hellcassius <caiorogerio.santos@gmail.com>
2021-06-11 20:41:20 -0300

    add notebook with generation of sample gps points

--- tests/test_gps_to_realized_trips.py
+++ tests/test_gps_to_realized_trips.py
@@ -19,6 +19,16 @@ def sample_gps_path(sample_data_path):
 
 
 @pytest.fixture
+def sample_gps_back_path(sample_data_path):
+    return sample_data_path / "simple_csv" / "simple_gps_back.csv"
+
+
+@pytest.fixture
+def sample_gps_roundtrip_path(sample_data_path):
+    return sample_data_path / "simple_csv" / "simple_gps_roundtrip.csv"
+
+
+@pytest.fixture
 def sample_gtfs_path(sample_data_path):
     return sample_data_path / "sample_gtfs"
 
@@ -39,6 +49,28 @@ def simple_rt_from_gps(sample_data_path):
     return df
 
 
+@pytest.fixture
+def simple_rt_from_gps_back(sample_data_path):
+    csv_path = sample_data_path / "simple_csv" / "simple_rt_from_gps_back.csv"
+    with open(csv_path, "r") as f:
+        df = pd.read_csv(f, parse_dates=["arrival_datetime", "departure_datetime"])
+    df["route_id"] = df["route_id"].astype("string")
+    df["trip_id"] = df["trip_id"].astype("string")
+    df["service_id"] = df["service_id"].astype("string")
+    return df
+
+
+@pytest.fixture
+def simple_rt_from_gps_roundtrip(sample_data_path):
+    csv_path = sample_data_path / "simple_csv" / "simple_rt_from_gps_both.csv"
+    with open(csv_path, "r") as f:
+        df = pd.read_csv(f, parse_dates=["arrival_datetime", "departure_datetime"])
+    df["route_id"] = df["route_id"].astype("string")
+    df["trip_id"] = df["trip_id"].astype("string")
+    df["service_id"] = df["service_id"].astype("string")
+    return df
+
+
 def test_gps_to_realized_trips(
     sample_gps_path, sample_gtfs_path, rgtfs_path, simple_rt_from_gps
 ):
@@ -47,3 +79,27 @@ def test_gps_to_realized_trips(
     rt["trip_id"] = rt["trip_id"].astype("string")
     rt["service_id"] = rt["service_id"].astype("string")
     assert rt.equals(simple_rt_from_gps)
+
+
+def test_gps_to_realized_trips_back(
+    sample_gps_back_path, sample_gtfs_path, rgtfs_path, simple_rt_from_gps_back
+):
+    rt = simple.main(sample_gtfs_path, sample_gps_back_path, rgtfs_path)
+    rt["route_id"] = rt["route_id"].astype("string")
+    rt["trip_id"] = rt["trip_id"].astype("string")
+    rt["service_id"] = rt["service_id"].astype("string")
+    assert rt.equals(simple_rt_from_gps_back)
+
+
+def test_gps_to_realized_trips_round(
+    sample_gps_roundtrip_path,
+    sample_gtfs_path,
+    rgtfs_path,
+    simple_rt_from_gps_roundtrip,
+):
+    rt = simple.main(sample_gtfs_path, sample_gps_roundtrip_path, rgtfs_path)
+    rt["route_id"] = rt["route_id"].astype("string")
+    rt["trip_id"] = rt["trip_id"].astype("string")
+    rt["service_id"] = rt["service_id"].astype("string")
+    assert rt.equals(simple_rt_from_gps_roundtrip)
+
